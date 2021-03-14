6b60c3de8074a1d669971e359b5f6275402e1ceb
Turlough <turloughcowman@gmail.com>
2021-03-14 17:25:05 +0000

    Refactoring and unit tests

--- code/main.py
+++ code/main.py
@@ -6,6 +6,6 @@ def prettify(s):
     return json.dumps(s, indent=4, sort_keys=True)
 
 
-api = ApiRequest(keyfile='./security/api_keys.json')
+api = ApiRequest(keyfile='../security/api_keys.json')
 j = api.request('accounts')
 print(prettify(j.json()))
