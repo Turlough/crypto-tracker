bc07a1a17fb72543840c22b7e50f234e6b9c91a0
Turlough <turloughcowman@gmail.com>
2021-03-14 17:20:04 +0000

    Refactoring and unit tests

--- tests/test_ApiRequest.py
+++ tests/test_ApiRequest.py
@@ -1,22 +1,16 @@
 from unittest import TestCase
 from hamcrest import *
 import json
-
 import sys
-
 sys.path.insert(1, '../code')
-
-
+sys.path.insert(1, '../security')
 from code.ApiRequest import ApiRequest
 
 
-class TestAuth(TestCase):
+class TestApiRequest(TestCase):
+
     api = ApiRequest(keyfile='../security/api_keys.json')
 
-    def prettify(s):
-        return json.dumps(s, indent=4, sort_keys=True)
-
-    def test_get_accounts(self):
-
+    def test_request(self):
         response = self.api.request('accounts')
         assert_that(response.status_code, is_(200))
